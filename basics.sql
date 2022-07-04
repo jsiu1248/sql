86bd49b0eb006058babfaf1d748e919c16177a29
Jonathan Siu <jsiu1248@gmail.com>
2022-07-03 20:25:40 -0700

    added notes of different languages for sql

--- basics.sql
+++ basics.sql
@@ -7,4 +7,27 @@ COMMIT;
 
 /* DDL - Data Definition Language: defining database structure or constraints
 Domain Constraints: values/type constraint
-Referential Integrity: */
+Referential Integrity: ensure value appears in relation i.e. department listed for each course must be
+one that actually exists
+Assertions: condition that database must satisfy.
+Authorization: different access i.e. read, insert, update
+CREATE, ALTER, DROP - delete objects from database
+, RENAME
+
+
+DML - Data Manipulation Language: managing data schema objects
+Procedural DLMS: specify what and how to get data
+Declarative DML (non-procedural DML): system figure out efficient mean of accessing data
+SELECT, INSERT, UPDATE,
+DELETE - delete all records from table
+
+TCL - Transactional Control Language: allows statements to be grouped
+COMMIT - permanently save transaction
+ROLLBACK - restore database to last commited state
+SAVEPOINT - temp save transaction
+
+DCL - Data Control Language: control access to data
+
+GRANT, REVOKE
+
+ */
