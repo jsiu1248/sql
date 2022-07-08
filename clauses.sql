8dba4f2734549889a43be675c55c6ca82a7a1108
Jonathan Siu <jsiu1248@gmail.com>
2022-07-07 18:56:06 -0700

    added notes on except

--- clauses.sql
+++ clauses.sql
@@ -7,3 +7,23 @@
         SELECT EmployeeID,Name, Salary 
         FROM Employee, temporaryTable 
         WHERE Employee.Salary > temporaryTable.averageValue;
+
+/*
+OFFSET - identify starting point from result set
+FETCH - used with offset to return set of rows
+Can't use ORDBER BY ....FETCH
+*/
+
+SELECT column_name(s)
+FROM table_name
+ORDER BY column_name
+OFFSET rows_to_skip
+FETCH NEXT number_of_rows ROWS ONLY;
+
+/*EXCEPT: returns from first SELECT and not the second
+Feels kind of like a setdiff
+EXCEPTALL - retains all of the duplicates
+
+EXCEPT VS NOT IN
+EXCEPT removes all duplicates in the final results vs
+NOT IN retains the duplicates*/
